cbbe2038b588bb3047b2d251f80ed70a36217994
Anthony Shorter <anthshor@gmail.com>
2015-09-20 17:16:52 +1200

    Grid Install

---
+++ Vagrantfile
@@ -0,0 +1,21 @@
+vm_name = "whister.zoomer"
+vm_disk_data = 1
+vm_disk_fra = 1
+Vagrant.configure(2) do |config|
+  vm_disk = vm_disk_data+vm_disk_fra
+  config.vm.box = "anthshor/ol6-oracledb"
+  config.vm.hostname = vm_name
+  config.vm.synced_folder "~/proxy", "/proxy"
+  config.vm.network "private_network", ip: "192.168.33.11" 
+  config.vm.provider :virtualbox do |vb|
+    vb.customize ['modifyvm', :id, '--memory', '4096']
+    vb.name=vm_name
+    (1..vm_disk).each do |i|
+      disk="asmdisk#{i}.vdi"
+      port=i
+      vb.customize ['createhd', '--filename', disk, '--size', '5128'] unless File.exists?(disk)
+      vb.customize ['storageattach', :id, '--storagectl', 'SATA Controller', '--port', port, '--device', '0', '--type', 'hdd', '--medium', disk]
+    end
+  end
+  config.vm.provision "shell",  path: "provision.sh"
+end
---
+++ provision.sh
@@ -0,0 +1,66 @@
+#!/bin/bash
+
+# define the global variables
+export STAGE="/u01/stage"
+export SOFTWARE="/u01/software"
+export PASSWORD="Password1#"
+export VERSION="12.1.0.2"
+
+# Proxy
+[ -f /proxy/.proxy.env ] && source /proxy/.proxy.env
+
+# Start NTP daemon
+service ntpd status && service ntpd restart || service ntpd start
+chkconfig ntpd on
+
+# Installing Grid Infrastructure Using a Software-Only Installation
+# NB /etc/resolv.conf - server can't find logitech: NXDOMAIN (skipped prereqs) - fix later
+if [ ! -f /u01/app/${VERSION}/grid/root.sh ]; then
+  echo "Installing Grid software..."
+  sudo -E -H -u grid /u01/stage/grid/runInstaller -silent -ignoreSysPrereqs  -ignorePrereq -waitforcompletion  \
+  oracle.install.asm.SYSASMPassword=oracle12 oracle.install.asm.monitorPassword=oracle12 \
+  ORACLE_HOSTNAME=logitech.sprite.zero \
+  INVENTORY_LOCATION=/u01/app/oraInventory \
+  SELECTED_LANGUAGES=en \
+  oracle.install.option=CRS_SWONLY \
+  ORACLE_BASE=/u01/app/grid \
+  ORACLE_HOME=/u01/app/12.1.0.2/grid \
+  oracle.install.asm.OSDBA=asmdba \
+  oracle.install.asm.OSOPER=asmoper \
+  oracle.install.asm.OSASM=asmadmin \
+  oracle.install.crs.config.ClusterType=STANDARD \
+  oracle.install.crs.config.gpnp.configureGNS=false \
+  oracle.install.crs.config.sharedFileSystemStorage.votingDiskRedundancy=NORMAL \
+  oracle.install.crs.config.sharedFileSystemStorage.ocrRedundancy=NORMAL \
+  oracle.install.crs.config.useIPMI=false \
+  oracle.install.crs.config.ignoreDownNodes=false \
+  oracle.install.config.managementOption=NONE 
+
+
+  /u01/app/oraInventory/orainstRoot.sh
+  /u01/app/${VERSION}/grid/root.sh
+
+# Configuring Grid for standalone
+/u01/app/12.1.0.2/grid/perl/bin/perl -I/u01/app/12.1.0.2/grid/perl/lib -I/u01/app/12.1.0.2/grid/crs/install /u01/app/12.1.0.2/grid/crs/install/roothas.pl
+
+else
+  echo "Skipping Grid Installation."
+fi
+
+pushd /dev
+  chown grid:dba sdb
+  chown grid:dba sdc
+popd
+
+# Run asmca
+# Create DATA and FRA
+# Need command line for scripting
+
+# Configure disks to use ASMFD
+
+# ./runInstaller for db software
+# Saved responsefile in /vagrant for command line scripting
+
+# dbca to create db
+
+# netca required?
